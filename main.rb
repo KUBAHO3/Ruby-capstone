ec0f6a47f82690d352c58387ee0c897a82515693
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-04 15:59:03 +0530

    Add connection methods

--- main.rb
+++ main.rb
@@ -1,21 +1,21 @@
-require './item.rb'
+require './item'
 
 def options
   puts 'Welcome to our Application'
   puts 'Please Choose from below options by number',
-    '1-List all books',
-    '2-List all music albums',
-    '3-List all movies',
-    '4-List of games',
-    '5-List all genres (e.g "Comedy", "Thriller")',
-    '6-List all labels (e.g. Gift, New)',
-    '7-List all authors (e.g. Stephen King)',
-    '8-List all sources (e.g. From a friend, Online shop)',
-    '9-Add a book',
-    '10-Add a music album',
-    '11-Add a movie',
-    '12-Add a game',
-    '13-Quit Application'
+       '1-List all books',
+       '2-List all music albums',
+       '3-List all movies',
+       '4-List of games',
+       '5-List all genres (e.g "Comedy", "Thriller")',
+       '6-List all labels (e.g. Gift, New)',
+       '7-List all authors (e.g. Stephen King)',
+       '8-List all sources (e.g. From a friend, Online shop)',
+       '9-Add a book',
+       '10-Add a music album',
+       '11-Add a movie',
+       '12-Add a game',
+       '13-Quit Application'
 end
 
 options
