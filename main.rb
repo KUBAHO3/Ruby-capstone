c631aa8fb96212180219e4533c65ac9f35ae92fe
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-04 19:15:34 +0530

    Reduce cyclomatic complexity

--- main.rb
+++ main.rb
@@ -29,23 +29,77 @@ def connection
   puts 'Thank you for using our App'
 end
 
+ACTIONS = {
+  1 => :list_books,
+  2 => :list_music_albums,
+  3 => :list_movies,
+  4 => :list_games,
+  5 => :list_genres,
+  6 => :list_labels,
+  7 => :list_authors,
+  8 => :list_sources,
+  9 => :add_book,
+  10 => :add_music,
+  11 => :add_movie,
+  12 => :add_game
+}.freeze
+
 def user_input(number)
-  case number
-  when 1 then list_books
-  when 2 then list_music_albums
-  when 3 then list_movies
-  when 4 then list_games
-  when 5 then list_genres
-  when 6 then list_labels
-  when 7 then list_authors
-  when 8 then list_sources
-  when 9 then add_book
-  when 10 then add_music
-  when 11 then add_movie
-  when 12 then add_game
+  action = ACTIONS[number]
+
+  if action
+    send(action)
   else
-    puts 'Invalid number Entered'
+    puts 'Invalid number entered'
   end
+end
+
+def list_books
+  puts 'book list'
+end
+
+def list_music_albums
+  # code to list all music albums
+end
+
+def list_movies
+  # code to list all movies
+end
+
+def list_games
+  # code to list all games
+end
+
+def list_genres
+  # code to list all genres
+end
+
+def list_labels
+  # code to list all labels
+end
+
+def list_authors
+  # code to list all authors
+end
+
+def list_sources
+  # code to list all sources
+end
+
+def add_book
+  # code to add a new book
+end
+
+def add_music
+  # code to add a new music album
+end
+
+def add_movie
+  # code to add a new movie
+end
+
+def add_game
+  # code to add a new game
 end
 
 connection
