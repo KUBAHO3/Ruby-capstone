ab848a09258dbd975565533e3c1aee41a189067a
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-04 21:24:23 +0530

    Genre Define & setup properties in constructor

--- genre.rb
+++ genre.rb
@@ -1,3 +1,17 @@
 class Genre
-  
+  attr_accessor :name
+
+  def initialize(name, items = [])
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = items
+  end
+
+  def add_item(item: item)
+  end
+
+  private
+
+  attr_reader :id
+  attr_accessor :items
 end
