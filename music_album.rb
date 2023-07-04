fb6ddfd7d1764c0c49a63122e586462a9a61cec2
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-04 22:25:31 +0530

    Implement add_item method in the MusicAlbum class

--- music_album.rb
+++ music_album.rb
@@ -1,4 +1,6 @@
-class MusicAlbum
+require './item'
+
+class MusicAlbum < Item
   attr_accessor :on_spotify
   
   def initialize(on_spotify)
@@ -8,6 +10,9 @@ class MusicAlbum
   private
 
   def can_be_archived?()
+    super
+    @can_be_archived? && @on_spotify ? true : false
+
   end
-  
+
 end
