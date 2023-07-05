a08b8e79be5ede20d4177763b29f7403fb5fc763
Joy Phoebe <joyapisi@gmail.com>
2023-07-05 14:39:12 +0300

    Fix linter errors

--- book.rb
+++ book.rb
@@ -1,4 +1,4 @@
-require_relative './item'
+require_relative 'item'
 
 class Book < Item
   attr_accessor :publisher, :cover_state
@@ -11,6 +11,6 @@ class Book < Item
   end
 
   def can_be_archived?
-    super || cover_state == "bad"
+    super || cover_state == 'bad'
   end
 end
