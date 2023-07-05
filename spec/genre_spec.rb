867f9fc8add5228bc6557e53694ce555fca029f8
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-05 22:13:09 +0530

    Add music_genre_op_spec for tests

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -4,6 +4,7 @@ describe Genre do
   before :each do
     @genre = Genre.new('Comedy')
     @genre.add_item('item1')
+    @genre.add_item('item2')
   end
 
   it 'should create a new Genre instance' do
@@ -17,8 +18,11 @@ describe Genre do
   end
 
   describe '#items' do
-    it 'should return the items of the Genre' do
+    it 'should return the item1 of the Genre' do
       expect(@genre.items[0]).to eq 'item1'
+    end
+    it 'should return the item2 of the Genre' do
+      expect(@genre.items[1]).to eq 'item2'
     end
   end
 end
