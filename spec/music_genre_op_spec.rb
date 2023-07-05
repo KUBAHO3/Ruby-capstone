caf1fa64729e3457f8589c4a5e1325a9506d33d6
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-05 22:13:52 +0530

    Fix linter errors

--- spec/music_genre_op_spec.rb
+++ spec/music_genre_op_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Operations do
   before :each do
-    @op = Operations.new()
+    @op = Operations.new
   end
 
   it 'should create a new Operations instance' do
@@ -11,16 +11,16 @@ describe Operations do
 
   describe '#genre' do
     it 'should return the genre of the Operations' do
-      @op.genre = ['Thriller', 'Romance']
+      @op.genre = %w[Thriller Romance]
       expect(@op.genre[0]).to eq 'Thriller'
     end
   end
 
   describe '#music_album' do
     it 'should return the music_album of the music_album' do
-        @op.music_album = [{name: 'My self', publish_date: '11/12/2015', on_spotify: true}]
-        tester = @op.music_album[0]
-        expect(tester[:on_spotify]).to eq true
+      @op.music_album = [{ name: 'My self', publish_date: '11/12/2015', on_spotify: true }]
+      tester = @op.music_album[0]
+      expect(tester[:on_spotify]).to eq true
     end
   end
 end
