6e6377fa4a01394544d462296b354f2b793ee3b7
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-05 11:50:41 +0530

    Add list music album function

---
+++ music_genre_op.rb
@@ -0,0 +1,18 @@
+require './genre'
+require './music_album'
+
+class Operations
+    attr_accessor :genre, :music_album
+
+    def initialize
+        @genre = []
+        @music_album = []
+    end
+
+    def list_music_albums
+        puts "Sorry your music album list is empty" if @music_album.empty?
+        @music_album.each_with_index do |album, index|
+            puts "(#{index}) Name: #{album.name} PublishedAt: #{album.published_date} Onspotify: #{album.on_spotify}"
+        end
+    end
+end
