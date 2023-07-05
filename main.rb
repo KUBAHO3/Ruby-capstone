0b71e7181bd88c0f8f075a9012e67bd72fe254bf
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-05 15:08:48 +0530

    Fix linter errors

--- main.rb
+++ main.rb
@@ -15,8 +15,8 @@ def options
        '10-Add a game',
        '11-Quit Application',
        '----------------------------------------------'
-      end
-      
+end
+
 def connection
   music_genre_operation = Operations.new
   loop do
@@ -29,46 +29,26 @@ def connection
   puts 'Thank you for using our App'
 end
 
-
 def user_input(method, number)
-  case number
-  when 1 then list_books
-  when 2 then method.list_music_albums
-  when 3 then list_games
-  when 4 then method.list_genres
-  when 5 then list_labels
-  when 6 then list_authors
-  when 7 then add_book
-  when 8 then method.add_music
-  when 9 then method.create_genre
-  when 10 then add_game
+  actions = {
+    1 => -> { puts 'This will list a book' },
+    2 => -> { method.list_music_albums },
+    3 => -> { puts 'This will list the games' },
+    4 => -> { method.list_genres },
+    5 => -> { puts 'This will list the labels' },
+    6 => -> { puts 'This will list the authours' },
+    7 => -> { puts 'This will add a book' },
+    8 => -> { method.add_music },
+    9 => -> { method.create_genre },
+    10 => -> { puts 'This will add a game' }
+  }
+
+  action = actions[number]
+  if action
+    action.call
   else
-    puts 'Invalid number Entered'
+    puts 'Invalid number entered'
   end
-end
-
-def list_books
-  puts 'book list'
-end
-
-def list_games
-  # code to list all games
-end
-
-def list_labels
-  # code to list all labels
-end
-
-def list_authors
-  # code to list all authors
-end
-
-def add_book
-  # code to add a new book
-end
-
-def add_game
-  # code to add a new game
 end
 
 connection
