b7f64a99158475bcc0fad9da7bd727a2ac883f08
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-05 11:56:21 +0530

    Add list genre function

--- music_genre_op.rb
+++ music_genre_op.rb
@@ -12,7 +12,14 @@ class Operations
     def list_music_albums
         puts "Sorry your music album list is empty" if @music_album.empty?
         @music_album.each_with_index do |album, index|
-            puts "(#{index}) Name: #{album.name} PublishedAt: #{album.published_date} Onspotify: #{album.on_spotify}"
+            puts "(#{index}) Name: '#{album.name}' PublishedAt: '#{album.published_date}' Onspotify: '#{album.on_spotify}'"
+        end
+    end
+
+    def list_genres
+        puts "Sorry your genre list is empty" if @genres.empty?
+        @genre.each_with_index do |genre, index|
+            puts "(#{index}) Name: '#{genre.name}'"
         end
     end
 end
