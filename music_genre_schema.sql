73f0c9da3dc4a47631e1760805b4c672c0a85591
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-06 15:01:09 +0530

    Add MusicAlbum table

--- music_genre_schema.sql
+++ music_genre_schema.sql
@@ -4,3 +4,13 @@ CREATE TABLE Genre(
   items VARCHAR,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE MusicAlbum(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  genre_id INT,
+  publish_date DATE,
+  archived BOOLEAN,
+  on_spotify BOOLEAN,
+  PRIMARY KEY (id),
+  FOREIGN KEY (genre_id) REFERENCES Genre(id)
+);
