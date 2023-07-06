c1eaf4bfba9b55c144073d271f61dca69a4a256f
Joy Phoebe <joyapisi@gmail.com>
2023-07-06 22:26:43 +0300

    Update book_spec test

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,19 +3,19 @@ require 'date'
 
 RSpec.describe Book do
   it 'creates a book' do
-    book = Book.new('Joy', 'bad', Date.parse('2010-08-13'))
+    book = Book.new('Joy', 'bad', { day: 31, month: 5, year: 2000 })
 
     expect(book.publisher).to eq('Joy')
   end
 
   it 'checks if the book is archivable' do
-    book1 = Book.new('Joy', 'bad', Date.parse('2010-08-13'))
+    book1 = Book.new('Joy', 'bad', { day: 31, month: 5, year: 2000 })
     expect(book1.can_be_archived?).to eq(true)
 
-    book2 = Book.new('Joy', 'Good', Date.parse('2018-08-13'))
+    book2 = Book.new('Joy', 'Good', { day: 31, month: 5, year: 2018 })
     expect(book2.can_be_archived?).to eq(false)
 
-    book3 = Book.new('Joy', 'Good', Date.parse('2010-10-10'))
+    book3 = Book.new('Joy', 'Good', { day: 31, month: 5, year: 2000 })
     expect(book3.can_be_archived?).to eq(true)
   end
 end
