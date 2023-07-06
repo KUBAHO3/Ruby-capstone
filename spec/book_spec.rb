fe7a50759a35cbe154e79d26f1ab7c39c7c9242b
Joy Phoebe <joyapisi@gmail.com>
2023-07-06 20:34:49 +0300

    update book-spec

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,24 +1,21 @@
 require_relative 'spec_helper'
+require 'date'
 
 RSpec.describe Book do
-  describe '#initialize' do
-    it 'creates a new book with publisher, cover state, and publish date' do
-      book = Book.new('Publisher', 'Cover State', Date.parse('2021-01-01'))
-      expect(book.publisher).to eq('Publisher')
-      expect(book.cover_state).to eq('Cover State')
-      expect(book.publish_date).to eq(Date.parse('2021-01-01'))
-    end
+  it 'creates a book' do
+    book = Book.new('Joy', 'bad', Date.parse('2010-08-13'))
+
+    expect(book.publisher).to eq('Joy')
   end
 
-  describe '#can_be_archived?' do
-    it 'returns true if the cover state is "bad"' do
-      book = Book.new('Publisher', 'bad', Date.parse('2021-01-01'))
-      expect(book.can_be_archived?).to be true
-    end
+  it 'checks if the book is archivable' do
+    book1 = Book.new('Joy', 'bad', Date.parse('2010-08-13'))
+    expect(book1.can_be_archived?).to eq(true)
 
-    it 'returns the result from the superclass if the cover state is not "bad"' do
-      book = Book.new('Publisher', 'new', Date.parse('2021-01-01'))
-      expect(book.can_be_archived?).to be false
-    end
+    book2 = Book.new('Joy', 'Good', Date.parse('2018-08-13'))
+    expect(book2.can_be_archived?).to eq(false)
+
+    book3 = Book.new('Joy', 'Good', Date.parse('2010-10-10'))
+    expect(book3.can_be_archived?).to eq(true)
   end
 end
