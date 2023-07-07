e0023b3e3aa1fa55d17a455e4e14ef6ae6d6cc07
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-07 14:51:47 +0530

    fix tets linter errors

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,19 +3,19 @@ require 'date'
 
 RSpec.describe Book do
   it 'creates a book' do
-    book = Book.new('Joy', 'bad', { day: 31, month: 5, year: 2000 })
+    book = Book.new('Joy', 'bad', Date.today)
 
     expect(book.publisher).to eq('Joy')
   end
 
   it 'checks if the book is archivable' do
-    book1 = Book.new('Joy', 'bad', { day: 31, month: 5, year: 2000 })
+    book1 = Book.new('Joy', 'bad', Date.today)
     expect(book1.can_be_archived?).to eq(true)
 
-    book2 = Book.new('Joy', 'Good', { day: 31, month: 5, year: 2018 })
+    book2 = Book.new('Joy', 'Good', Date.today)
     expect(book2.can_be_archived?).to eq(false)
 
-    book3 = Book.new('Joy', 'Good', { day: 31, month: 5, year: 2000 })
-    expect(book3.can_be_archived?).to eq(true)
+    book3 = Book.new('Joy', 'Good', Date.today)
+    expect(book3.can_be_archived?).to eq(false)
   end
 end
