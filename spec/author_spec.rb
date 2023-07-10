6c4c20d74a630de255d8943666bfe129657aac21
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-07-11 00:00:22 +0530

    Modify the import statements from spec files

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,5 +1,5 @@
-require_relative '../author'
-require_relative '../item'
+require_relative '../classes/author'
+require_relative '../classes/item'
 
 RSpec.describe Author do
   before :each do
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../game'
+require_relative '../classes/game'
 
 RSpec.describe Game do
   describe '#initialize' do
